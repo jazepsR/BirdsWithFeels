289c83bb0f9277279f8769ee753394c133e29c5f
Kristaps <mtdr@inbox.lv>
2017-04-04 23:12:50 +0300

    - drag buttons?

---
+++ Assets/Scripts/LayoutButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LayoutButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler 
+{
+	public Vector2 index = new Vector2(0,0);
+	private Vector2 nopeVec = new Vector2 (-1, -1);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		Debug.Log ("OnPointerExit:" + index);
+		GameLogic.Instance.dropVector = nopeVec;
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		Debug.Log ("OnPointerEnter:" + index);
+		GameLogic.Instance.dropVector = index;
+	}
+}
