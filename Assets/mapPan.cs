b4f225a69dd4d4f7cc82cddb1cafb781d1c2e819
Jazeps Rutkis <jazepsrutkis93@gmail.com>
2017-07-28 12:00:34 +0300

    Map panning fix

--- Assets/mapPan.cs
+++ Assets/mapPan.cs
@@ -1,24 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class mapPan : MonoBehaviour {
+public class mapPan : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
     public float mouseSensitivity;
     private Vector3 lastPosition;
-
+    public bool canMove = true;
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && canMove && Var.selectedBird == null)
         {
             lastPosition = Input.mousePosition;
         }
 
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) && canMove && Var.selectedBird == null)
         {
             var delta = Input.mousePosition - lastPosition;
             transform.Translate(delta.x * mouseSensitivity, delta.y * mouseSensitivity, 0);
             lastPosition = Input.mousePosition;
         }
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        canMove = true;
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        canMove = false;
     }
 }
