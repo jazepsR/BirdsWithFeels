5834486d853747d896af9a0a90b70560ad5ee527
Sebastian Larsson <Sebastian.Larsson.3738@student.uu.se>
2017-07-31 08:49:41 +0200

    Further tooltip experimentation. Still not 100% done!

--- Assets/Scripts/tooltipScript.cs
+++ Assets/Scripts/tooltipScript.cs
@@ -7,7 +7,7 @@ public class tooltipScript : MonoBehaviour {
     public void SetPos()
     {
         gameObject.GetComponent<Image>().enabled = true;
-        gameObject.GetComponentInChildren<Text>().color = Color.white;
+        gameObject.GetComponentInChildren<Text>().color = Color.black;
         var pos = transform.localPosition;
         RectTransform me = gameObject.GetComponent<RectTransform>();
 
