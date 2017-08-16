44e5e82924f43c3e86b418c3e1dd6c2c40dfb037
Sebastian Larsson <Sebastian.Larsson.3738@student.uu.se>
2017-08-16 12:18:35 +0200

    New forest background! Also organized animations a bit

---
+++ Assets/animatorOffsetScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animatorOffsetScript : MonoBehaviour {
+
+    Animator anime;
+    public float offset;
+
+	// Use this for initialization
+	void Start () {
+        anime = GetComponent<Animator>();
+        if (offset == 0)
+        {
+            offset = Random.Range(0, 50)/10;
+        }
+
+        anime.SetFloat("offset", offset);
+        print( offset);
+	}
+
+}
