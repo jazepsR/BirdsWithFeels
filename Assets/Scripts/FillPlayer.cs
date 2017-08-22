6e13d39c3f43bc2d6432234531ea5dfe5cec8f3f
Jazeps Rutkis <jazepsrutkis93@gmail.com>
2017-08-22 16:54:52 +0300

    feedback is above all other objects

--- Assets/Scripts/FillPlayer.cs
+++ Assets/Scripts/FillPlayer.cs
@@ -3,220 +3,221 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FillPlayer : MonoBehaviour {
-    public Bird[] playerBirds;
-    public Bird[] deadBirds;
-    public bool inMap = false;
-    public static FillPlayer Instance { get; private set; }
+	public Bird[] playerBirds;
+	public Bird[] deadBirds;
+	public bool inMap = false;
+	public static FillPlayer Instance { get; private set; }
 	// Use this for initialization
 	void Awake ()
-    {
-        LoadSprites();
-        Instance = this;
-        if (Var.isTutorial && !inMap)
-            return;
-        if (inMap && Var.availableBirds.Count>0)
-        {
-            foreach (Bird bird in Var.availableBirds)
-            {
-                bool wasActive = false;
-                foreach (Bird ActiveBird in Var.activeBirds)
-                {
-                    if (ActiveBird.charName == bird.charName)
-                    {
-                        wasActive = true;
-                        break;
-                    }
-                }
-                if (wasActive)
-                    bird.AdventuresRested = 0;
-                else
-                {
-                    if(!Var.fled)
-                        bird.AdventuresRested++;
-                }
-            }
-            foreach (Bird bird in playerBirds)
-            {
-                bool foundBird = false;
-                foreach (Bird loadBird in Var.availableBirds)
-                {
-                    if (bird.charName == loadBird.charName)
-                    {
-                        SetupBird(bird, loadBird);
-                        foundBird = true;
-                        break;
-                    }
-                }
-                if (!foundBird)
-                    bird.gameObject.SetActive(false);
-            }
-            
-        }
-        if (Var.availableBirds.Count < 1 && inMap)
-        {           
-            foreach(Bird bird in playerBirds)
-            {
-                if(bird.gameObject.activeSelf)
-                    Var.availableBirds.Add(bird);
-            }          
-        }
-     
-        
-        if (!inMap && Var.activeBirds.Count > 0)
-        {
-            for (int i = 0; i < Var.activeBirds.Count; i++)
-            {
-                SetupBird(playerBirds[i], Var.activeBirds[i]);
-            }
-            List<Bird> inactive = Helpers.Instance.GetInactiveBirds();
-            for(int i=0; i < inactive.Count; i++)
-            {
-                SetupBird(deadBirds[i], inactive[i]);
-            }
-        }
-       if (!inMap && Var.activeBirds.Count < 1)
-        {
-            Var.activeBirds.AddRange(playerBirds);
-        }
-    }
+	{
+		LoadSprites();
+		Instance = this;
+		if (Var.isTutorial && !inMap)
+			return;
+		if (inMap && Var.availableBirds.Count>0)
+		{
+			foreach (Bird bird in Var.availableBirds)
+			{
+				bool wasActive = false;
+				foreach (Bird ActiveBird in Var.activeBirds)
+				{
+					if (ActiveBird.charName == bird.charName)
+					{
+						wasActive = true;
+						break;
+					}
+				}
+				if (wasActive)
+					bird.AdventuresRested = 0;
+				else
+				{
+					if(!Var.fled)
+						bird.AdventuresRested++;
+				}
+			}
+			foreach (Bird bird in playerBirds)
+			{
+				bool foundBird = false;
+				foreach (Bird loadBird in Var.availableBirds)
+				{
+					if (bird.charName == loadBird.charName)
+					{
+						SetupBird(bird, loadBird);
+						foundBird = true;
+						break;
+					}
+				}
+				if (!foundBird)
+					bird.gameObject.SetActive(false);
+			}
+			
+		}
+		if (Var.availableBirds.Count < 1 && inMap)
+		{           
+			foreach(Bird bird in playerBirds)
+			{
+				if(bird.gameObject.activeSelf)
+					Var.availableBirds.Add(bird);
+			}          
+		}
+	 
+		
+		if (!inMap && Var.activeBirds.Count > 0)
+		{
+			for (int i = 0; i < Var.activeBirds.Count; i++)
+			{
+				SetupBird(playerBirds[i], Var.activeBirds[i]);
+			}
+			List<Bird> inactive = Helpers.Instance.GetInactiveBirds();
+			for(int i=0; i < inactive.Count; i++)
+			{
+				SetupBird(deadBirds[i], inactive[i]);
+			}
+		}
+	   if (!inMap && Var.activeBirds.Count < 1)
+		{
+			Var.activeBirds.AddRange(playerBirds);
+            Var.availableBirds.AddRange(playerBirds);
+		}
+	}
 
 	void Start()
-    {
-        if (Var.isTutorial&& !inMap)
-        {
-            for(int i =0;i<playerBirds.Length; i++)
-            {
-                if (i < Tutorial.Instance.BirdCount[0])
-                {
-                    playerBirds[i].gameObject.SetActive(true);
-                }else
-                {
-                    playerBirds[i].gameObject.SetActive(false);
-                }
-            }
-        }
-    }
+	{
+		if (Var.isTutorial&& !inMap)
+		{
+			for(int i =0;i<playerBirds.Length; i++)
+			{
+				if (i < Tutorial.Instance.BirdCount[0])
+				{
+					playerBirds[i].gameObject.SetActive(true);
+				}else
+				{
+					playerBirds[i].gameObject.SetActive(false);
+				}
+			}
+		}
+	}
 	void LoadSprites()
-    {
-        if (Var.lvlSprites == null)
-            Var.lvlSprites = Resources.LoadAll<Sprite>("Icons/NewIcons");
-        if (Var.skillIcons == null)
-            Var.skillIcons = Resources.LoadAll<Sprite>("sprites/skill_pictures");
-        if (Var.startingLvlSprites == null)
-            Var.startingLvlSprites = Resources.LoadAll<Sprite>("Icons/icons_startingabilties");
-        if (Var.hatSprites == null)
-        {
-            Var.hatSprites = new List<Sprite>();
-            Var.hatSprites.AddRange(Resources.LoadAll<Sprite>("sprites/hat_spriteSheet"));
-            Var.hatSprites.Add(Resources.Load<Sprite>("sprites/hat_spriteSheet_3"));
-            Var.hatSprites.Add(Resources.Load<Sprite>("sprites/hat_spriteSheet_4"));
-            // print(Var.hatSprites.Count);
-        }
-        if (Var.enemySprites == null)
-        {
-            Var.enemySprites = new List<GameObject>();
-            Var.enemySprites.Add(Resources.Load<GameObject>("prefabs/enemies/Visuals_side_confident"));
-            Var.enemySprites.Add(Resources.Load<GameObject>("prefabs/enemies/Visuals_side_friendly"));
-            Var.enemySprites.Add(Resources.Load<GameObject>("prefabs/enemies/Visuals_side_lonely"));
-            Var.enemySprites.Add(Resources.Load<GameObject>("prefabs/enemies/Visuals_side_neutral"));
-            Var.enemySprites.Add(Resources.Load<GameObject>("prefabs/enemies/Visuals_side_scared"));
-            Var.enemySprites.Add(Resources.Load<GameObject>("prefabs/enemies/Visuals_top_confident"));
-            Var.enemySprites.Add(Resources.Load<GameObject>("prefabs/enemies/Visuals_top_friendly"));
-            Var.enemySprites.Add(Resources.Load<GameObject>("prefabs/enemies/Visuals_top_lonely"));
-            Var.enemySprites.Add(Resources.Load<GameObject>("prefabs/enemies/Visuals_top_neutral"));
-            Var.enemySprites.Add(Resources.Load<GameObject>("prefabs/enemies/Visuals_top_scared"));
-        }
-    }
+	{
+		if (Var.lvlSprites == null)
+			Var.lvlSprites = Resources.LoadAll<Sprite>("Icons/NewIcons");
+		if (Var.skillIcons == null)
+			Var.skillIcons = Resources.LoadAll<Sprite>("sprites/skill_pictures");
+		if (Var.startingLvlSprites == null)
+			Var.startingLvlSprites = Resources.LoadAll<Sprite>("Icons/icons_startingabilties");
+		if (Var.hatSprites == null)
+		{
+			Var.hatSprites = new List<Sprite>();
+			Var.hatSprites.AddRange(Resources.LoadAll<Sprite>("sprites/hat_spriteSheet"));
+			Var.hatSprites.Add(Resources.Load<Sprite>("sprites/hat_spriteSheet_3"));
+			Var.hatSprites.Add(Resources.Load<Sprite>("sprites/hat_spriteSheet_4"));
+			// print(Var.hatSprites.Count);
+		}
+		if (Var.enemySprites == null)
+		{
+			Var.enemySprites = new List<GameObject>();
+			Var.enemySprites.Add(Resources.Load<GameObject>("prefabs/enemies/Visuals_side_confident"));
+			Var.enemySprites.Add(Resources.Load<GameObject>("prefabs/enemies/Visuals_side_friendly"));
+			Var.enemySprites.Add(Resources.Load<GameObject>("prefabs/enemies/Visuals_side_lonely"));
+			Var.enemySprites.Add(Resources.Load<GameObject>("prefabs/enemies/Visuals_side_neutral"));
+			Var.enemySprites.Add(Resources.Load<GameObject>("prefabs/enemies/Visuals_side_scared"));
+			Var.enemySprites.Add(Resources.Load<GameObject>("prefabs/enemies/Visuals_top_confident"));
+			Var.enemySprites.Add(Resources.Load<GameObject>("prefabs/enemies/Visuals_top_friendly"));
+			Var.enemySprites.Add(Resources.Load<GameObject>("prefabs/enemies/Visuals_top_lonely"));
+			Var.enemySprites.Add(Resources.Load<GameObject>("prefabs/enemies/Visuals_top_neutral"));
+			Var.enemySprites.Add(Resources.Load<GameObject>("prefabs/enemies/Visuals_top_scared"));
+		}
+	}
 	public static void SetupBird(Bird target, Bird template)
-    {
-        target.birdPrefabName = template.birdPrefabName;
-        target.charName = template.charName;
-        target.friendliness = template.friendliness;
-        target.confidence = template.confidence;
-        target.portraitOrder = template.portraitOrder;
-        target.health = template.health;
-        if (template.health < 1)
-            target.gameObject.GetComponent<Animator>().SetBool("dead", true);
-        target.portrait = template.portrait;
-        target.maxHealth = template.maxHealth;
-        target.levelList = template.levelList;
-        target.startingLVL = template.startingLVL;
-        target.battleCount = template.battleCount;
-        target.lastLevel = template.lastLevel;
-        target.level = template.level;
-        target.birdAbility = template.birdAbility;
-        target.consecutiveFightsWon = template.consecutiveFightsWon;
-        target.battlesToNextLVL = template.battlesToNextLVL;        
-        target.roundsRested = template.roundsRested;
-        target.AdventuresRested = template.AdventuresRested;
-        target.CoolDownLeft = template.CoolDownLeft;
-        target.CoolDownLength = template.CoolDownLength;
-        target.relationships = template.relationships;
-        target.preferredEmotion = template.preferredEmotion;
-       // target.transform.Find("BIRB_sprite/hat").GetComponent<SpriteRenderer>().sprite = template.hatSprite;
-    }
+	{
+		target.birdPrefabName = template.birdPrefabName;
+		target.charName = template.charName;
+		target.friendliness = template.friendliness;
+		target.confidence = template.confidence;
+		target.portraitOrder = template.portraitOrder;
+		target.health = template.health;
+		if (template.health < 1)
+			target.gameObject.GetComponent<Animator>().SetBool("dead", true);
+		target.portrait = template.portrait;
+		target.maxHealth = template.maxHealth;
+		target.levelList = template.levelList;
+		target.startingLVL = template.startingLVL;
+		target.battleCount = template.battleCount;
+		target.lastLevel = template.lastLevel;
+		target.level = template.level;
+		target.birdAbility = template.birdAbility;
+		target.consecutiveFightsWon = template.consecutiveFightsWon;
+		target.battlesToNextLVL = template.battlesToNextLVL;        
+		target.roundsRested = template.roundsRested;
+		target.AdventuresRested = template.AdventuresRested;
+		target.CoolDownLeft = template.CoolDownLeft;
+		target.CoolDownLength = template.CoolDownLength;
+		target.relationships = template.relationships;
+		target.preferredEmotion = template.preferredEmotion;
+	   // target.transform.Find("BIRB_sprite/hat").GetComponent<SpriteRenderer>().sprite = template.hatSprite;
+	}
 
 
-    public static BirdSaveData SetupSaveBird(Bird template)
-    {
-        BirdSaveData target = new BirdSaveData();
-        target.charName = template.charName;
-        target.friendliness = template.friendliness;
-        target.confidence = template.confidence;
-        target.portraitOrder = template.portraitOrder;
-        target.health = template.health;
-        target.birdPrefabName = template.birdPrefabName;
-        //target.portrait = template.portrait;
-        target.levelList = template.levelList;
-        target.startingLVL = template.startingLVL;
-        target.battleCount = template.battleCount;
-        target.lastLevel = template.lastLevel;
-        target.level = template.level;
-        target.birdAbility = template.birdAbility;
-        target.consecutiveFightsWon = template.consecutiveFightsWon;
-        target.battlesToNextLVL = template.battlesToNextLVL;
-        target.roundsRested = template.roundsRested;
-        target.AdventuresRested = template.AdventuresRested;
-        target.CoolDownLeft = template.CoolDownLeft;
-        target.CoolDownLength = template.CoolDownLength;
+	public static BirdSaveData SetupSaveBird(Bird template)
+	{
+		BirdSaveData target = new BirdSaveData();
+		target.charName = template.charName;
+		target.friendliness = template.friendliness;
+		target.confidence = template.confidence;
+		target.portraitOrder = template.portraitOrder;
+		target.health = template.health;
+		target.birdPrefabName = template.birdPrefabName;
+		//target.portrait = template.portrait;
+		target.levelList = template.levelList;
+		target.startingLVL = template.startingLVL;
+		target.battleCount = template.battleCount;
+		target.lastLevel = template.lastLevel;
+		target.level = template.level;
+		target.birdAbility = template.birdAbility;
+		target.consecutiveFightsWon = template.consecutiveFightsWon;
+		target.battlesToNextLVL = template.battlesToNextLVL;
+		target.roundsRested = template.roundsRested;
+		target.AdventuresRested = template.AdventuresRested;
+		target.CoolDownLeft = template.CoolDownLeft;
+		target.CoolDownLength = template.CoolDownLength;
 
-        return target;       
-    }
-    public static Bird LoadSavedBird(BirdSaveData template)
-    {
-        Bird target = new Bird("steve");
-        target.charName = template.charName;
-        target.friendliness = template.friendliness;
-        target.confidence = template.confidence;
-        target.portraitOrder = template.portraitOrder;
-        target.health = template.health;
-        target.birdPrefabName = template.birdPrefabName;
-        //target.portrait = template.portrait;
-        target.levelList = template.levelList;
-        target.startingLVL = template.startingLVL;
-        target.battleCount = template.battleCount;
-        target.lastLevel = template.lastLevel;
-        target.level = template.level;
-        target.birdAbility = template.birdAbility;
-        target.consecutiveFightsWon = template.consecutiveFightsWon;
-        target.battlesToNextLVL = template.battlesToNextLVL;
-        target.roundsRested = template.roundsRested;
-        target.AdventuresRested = template.AdventuresRested;
-        target.CoolDownLeft = template.CoolDownLeft;
-        target.CoolDownLength = template.CoolDownLength;
-        try
-        {
-            foreach (LevelData data in target.levelList)
-            {
-                data.LVLIcon = Helpers.Instance.GetLVLSprite(data.type);
-            }
-        }
-        catch { }
-        target.portrait = Resources.Load<GameObject>("prefabs/portrait_" + target.charName);
-        return target;
-    }
-    // Update is called once per frame
-    void Update () {
+		return target;       
+	}
+	public static Bird LoadSavedBird(BirdSaveData template)
+	{
+		Bird target = new Bird("steve");
+		target.charName = template.charName;
+		target.friendliness = template.friendliness;
+		target.confidence = template.confidence;
+		target.portraitOrder = template.portraitOrder;
+		target.health = template.health;
+		target.birdPrefabName = template.birdPrefabName;
+		//target.portrait = template.portrait;
+		target.levelList = template.levelList;
+		target.startingLVL = template.startingLVL;
+		target.battleCount = template.battleCount;
+		target.lastLevel = template.lastLevel;
+		target.level = template.level;
+		target.birdAbility = template.birdAbility;
+		target.consecutiveFightsWon = template.consecutiveFightsWon;
+		target.battlesToNextLVL = template.battlesToNextLVL;
+		target.roundsRested = template.roundsRested;
+		target.AdventuresRested = template.AdventuresRested;
+		target.CoolDownLeft = template.CoolDownLeft;
+		target.CoolDownLength = template.CoolDownLength;
+		try
+		{
+			foreach (LevelData data in target.levelList)
+			{
+				data.LVLIcon = Helpers.Instance.GetLVLSprite(data.type);
+			}
+		}
+		catch { }
+		target.portrait = Resources.Load<GameObject>("prefabs/portrait_" + target.charName);
+		return target;
+	}
+	// Update is called once per frame
+	void Update () {
 		
 	}
 }
--- Assets/Scripts/feedBack.cs
+++ Assets/Scripts/feedBack.cs
@@ -4,26 +4,27 @@ using UnityEngine;
 
 public class feedBack : MonoBehaviour {
 	public TextMesh feedBackText;
-    public TextMesh LvlIndicatorText;
-    public SpriteRenderer BelowBirdIndicator;
+	public TextMesh LvlIndicatorText;
+	public SpriteRenderer BelowBirdIndicator;
 	public Bird birdScript;
-    [HideInInspector]
-    public Bird PlayerEnemyBird = null;
+	[HideInInspector]
+	public Bird PlayerEnemyBird = null;
 	Bird.dir dir;
 	public float hideBonus;
 	public float hideVal = 0.3f;
 	public int myIndex;
 	string toolTipText;
-    battleFeedback myBattleFeedback;
-    Vector3 scale;  
+	battleFeedback myBattleFeedback;
+	Vector3 scale;  
 	// Use this for initialization
-	void Awake () {        
-        scale= BelowBirdIndicator.transform.localScale;
-        myBattleFeedback = feedBackText.gameObject.GetComponent<battleFeedback>();
-        myBattleFeedback.fb = this;		
+	void Awake () {
+        feedBackText.gameObject.GetComponent<Renderer>().sortingLayerName = "front";    
+		scale= BelowBirdIndicator.transform.localScale;
+		myBattleFeedback = feedBackText.gameObject.GetComponent<battleFeedback>();
+		myBattleFeedback.fb = this;		
 		dir = birdScript.position;
 		feedBackText.transform.localScale = Vector3.zero;       
-    }
+	}
 
 	//checks if the enemy has an opponent
 	public bool CheckOpponent()
@@ -65,61 +66,61 @@ public class feedBack : MonoBehaviour {
 	{
 		
 		hideBonus = 0.0f;
-        //print(birdScript.charName);
-        bool hasFeedback = false;
-            switch (dir)
-            {
-                case Bird.dir.top:
-                    for (int i = 0; i < 4; i++)
-                    {
-                        if (Var.playerPos[myIndex, i] != null)
-                        {
-                            Bird bird = Var.playerPos[myIndex, i];
-                            if (bird.isHiding)
-                            {
-                                hideBonus = hideVal;
-                            }
-                            else
-                            {
-                                PlayerEnemyBird = Var.playerPos[myIndex, i];
-                                ShowFeedback(GameLogic.Instance.GetBonus(Var.playerPos[myIndex, i], birdScript), Var.playerPos[myIndex, i]);
-                                hasFeedback = true;
-                                break;
-                            }
-                        }
+		//print(birdScript.charName);
+		bool hasFeedback = false;
+			switch (dir)
+			{
+				case Bird.dir.top:
+					for (int i = 0; i < 4; i++)
+					{
+						if (Var.playerPos[myIndex, i] != null)
+						{
+							Bird bird = Var.playerPos[myIndex, i];
+							if (bird.isHiding)
+							{
+								hideBonus = hideVal;
+							}
+							else
+							{
+								PlayerEnemyBird = Var.playerPos[myIndex, i];
+								ShowFeedback(GameLogic.Instance.GetBonus(Var.playerPos[myIndex, i], birdScript), Var.playerPos[myIndex, i]);
+								hasFeedback = true;
+								break;
+							}
+						}
 
-                    }
-                    break;
-                case Bird.dir.front:
-                    for (int i = 0; i < 4; i++)
-                    {
+					}
+					break;
+				case Bird.dir.front:
+					for (int i = 0; i < 4; i++)
+					{
 
-                        if (Var.playerPos[3 - i, myIndex] != null)
-                        {
-                            Bird bird = Var.playerPos[3 - i, myIndex];
-                            if (bird.isHiding)
-                            {
-                                hideBonus = hideVal;
-                            }
-                            else
-                            {
-                                PlayerEnemyBird = Var.playerPos[3 - i, myIndex];
-                                ShowFeedback(GameLogic.Instance.GetBonus(Var.playerPos[3 - i, myIndex], birdScript), Var.playerPos[3 - i, myIndex]);
-                                hasFeedback = true;
-                                break;
-                            }
-                        }
+						if (Var.playerPos[3 - i, myIndex] != null)
+						{
+							Bird bird = Var.playerPos[3 - i, myIndex];
+							if (bird.isHiding)
+							{
+								hideBonus = hideVal;
+							}
+							else
+							{
+								PlayerEnemyBird = Var.playerPos[3 - i, myIndex];
+								ShowFeedback(GameLogic.Instance.GetBonus(Var.playerPos[3 - i, myIndex], birdScript), Var.playerPos[3 - i, myIndex]);
+								hasFeedback = true;
+								break;
+							}
+						}
 
-                    }
-                    break;
-               
-            }
-        if(!hasFeedback)
-            HideFeedBack(false);
+					}
+					break;
+			   
+			}
+		if(!hasFeedback)
+			HideFeedBack(false);
 
 
 
-    }
+	}
 
 	public bool CheckResting(Bird bird)
 	{
@@ -157,35 +158,35 @@ public class feedBack : MonoBehaviour {
 
 	public void SetEnemyHoverText()
 	{
-        BelowBirdIndicator.gameObject.SetActive(true);
-        BelowBirdIndicator.color = new Color(1, 1, 1, 1f);        
-        string name = Helpers.Instance.GetName(Helpers.Instance.RandomBool());
-        LvlIndicatorText.text = (birdScript.levelRollBonus + 1).ToString();
-        string strength = "None";
+		BelowBirdIndicator.gameObject.SetActive(true);
+		BelowBirdIndicator.color = new Color(1, 1, 1, 1f);        
+		string name = Helpers.Instance.GetName(Helpers.Instance.RandomBool());
+		LvlIndicatorText.text = (birdScript.levelRollBonus + 1).ToString();
+		string strength = "None";
 		string weakness = "All";
 		if (Helpers.Instance.GetStenght(birdScript.emotion) != Var.Em.Neutral)
 			strength = Helpers.Instance.GetHexColor(Helpers.Instance.GetStenght(birdScript.emotion)) + Helpers.Instance.GetStenght(birdScript.emotion).ToString() + "</color>";
-        if (Helpers.Instance.GetWeakness(birdScript.emotion) != Var.Em.Neutral)
-            weakness = Helpers.Instance.GetHexColor(Helpers.Instance.GetWeakness(birdScript.emotion)) + Helpers.Instance.GetWeakness(birdScript.emotion).ToString() + "</color>";
+		if (Helpers.Instance.GetWeakness(birdScript.emotion) != Var.Em.Neutral)
+			weakness = Helpers.Instance.GetHexColor(Helpers.Instance.GetWeakness(birdScript.emotion)) + Helpers.Instance.GetWeakness(birdScript.emotion).ToString() + "</color>";
 		toolTipText = name + "- " + birdScript.emotion + "\nStrength " + (birdScript.levelRollBonus+1).ToString() + "\n Weak to: " + weakness + "\nStrong against: " + strength; 
 	}
-    void ScaleDownIndicator()
-    {
-        LeanTween.scale(BelowBirdIndicator.gameObject, scale, 0.2f).setEase(LeanTweenType.easeInBack);       
-    }
+	void ScaleDownIndicator()
+	{
+		LeanTween.scale(BelowBirdIndicator.gameObject, scale, 0.2f).setEase(LeanTweenType.easeInBack);       
+	}
 	public void ShowEnemyHoverText()
 	{
-        Helpers.Instance.ShowTooltip(toolTipText);
-    }
+		Helpers.Instance.ShowTooltip(toolTipText);
+	}
 	
 	public void ShowFeedback(float value,Bird bird)
 	{
-        if (!BelowBirdIndicator.color.Equals(new Color(0.5f, 0, 0, 1f)))
-        {          
-            BelowBirdIndicator.color = new Color(0.5f, 0, 0, 1f);
-            LeanTween.scale(BelowBirdIndicator.gameObject, scale * 1.2f, 0.15f).setEase(LeanTweenType.easeOutCubic).setOnComplete(ScaleDownIndicator);
-        }
-        bird.fighting = true;
+		if (!BelowBirdIndicator.color.Equals(new Color(0.5f, 0, 0, 1f)))
+		{          
+			BelowBirdIndicator.color = new Color(0.5f, 0, 0, 1f);
+			LeanTween.scale(BelowBirdIndicator.gameObject, scale * 1.2f, 0.15f).setEase(LeanTweenType.easeOutCubic).setOnComplete(ScaleDownIndicator);
+		}
+		bird.fighting = true;
 		feedBackText.gameObject.SetActive(true);
 		//float colorIndex = (value + 4.0f) / 8;
 		value = Mathf.Clamp01(value+hideBonus);
@@ -196,22 +197,22 @@ public class feedBack : MonoBehaviour {
 		//feedBackText.text = ((int)(value*100)).ToString("+#;-#;0") +" %";
 		feedBackText.text =(Mathf.Ceil(value * 100)).ToString("0");
 	}
-    
+	
 	public void HideFeedBack(bool forFight)
 	{
-        if (forFight) {
-            BelowBirdIndicator.gameObject.SetActive(false);
-        } else
-        {
-            if (!BelowBirdIndicator.color.Equals(new Color(1, 1, 1, 1f)) )
-            {
-                             
-                BelowBirdIndicator.color = new Color(1, 1, 1, 1f);
-                LeanTween.scale(BelowBirdIndicator.gameObject, scale * 1.2f, 0.15f).setEase(LeanTweenType.easeOutCubic).setOnComplete(ScaleDownIndicator);
-            }
-        }
+		if (forFight) {
+			BelowBirdIndicator.gameObject.SetActive(false);
+		} else
+		{
+			if (!BelowBirdIndicator.color.Equals(new Color(1, 1, 1, 1f)) )
+			{
+							 
+				BelowBirdIndicator.color = new Color(1, 1, 1, 1f);
+				LeanTween.scale(BelowBirdIndicator.gameObject, scale * 1.2f, 0.15f).setEase(LeanTweenType.easeOutCubic).setOnComplete(ScaleDownIndicator);
+			}
+		}
 
-        LeanTween.scale(feedBackText.gameObject, Vector3.zero, 0.3f).setEase(LeanTweenType.easeInOutBack);
+		LeanTween.scale(feedBackText.gameObject, Vector3.zero, 0.3f).setEase(LeanTweenType.easeInOutBack);
 
 	}
 
