0acc2d3ca75be079bdc81b82a402cfb9eee21873
Jazeps Rutkis <jazepsrutkis93@gmail.com>
2017-09-13 10:41:59 +0300

    implemented new firendship lines

--- Assets/Scripts/firendLine.cs
+++ Assets/Scripts/firendLine.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 [System.Serializable]
 public class firendLine : MonoBehaviour {
-	public GameObject LineObj;
+	//public GameObject LineObj;
 	public List<GameObject> activeLines = new List<GameObject>();
 	public Color thick;
 	public Color thin;
@@ -11,9 +11,23 @@ public class firendLine : MonoBehaviour {
 	public Color relationship;
     public LineRenderer lr;
 	Bird birdScript;
+    public static GameObject horizontalLine = null;
+    public static GameObject verticalLine;
+    public static GameObject diognalLineShort;
+    public static GameObject diognalLineLong;
 	// Use this for initialization
 	void Start () {
-		birdScript = GetComponent<Bird>();        
+		birdScript = GetComponent<Bird>(); 
+        if(horizontalLine == null)
+        {
+            horizontalLine = Resources.Load<GameObject>("prefabs/lines/horizontal");
+            verticalLine = Resources.Load<GameObject>("prefabs/lines/vertical");
+            diognalLineShort = Resources.Load<GameObject>("prefabs/lines/diagShort");
+            diognalLineLong =  Resources.Load<GameObject>("prefabs/lines/diagLong");
+
+
+        }
+               
 	}
 	
 	// Update is called once per frame
@@ -26,19 +40,19 @@ public class firendLine : MonoBehaviour {
 	{
 		if (x < 0 || y < 0)
 			return;
-		DrawLine(y, x + 1,true);
-		DrawLine(y, x - 1,true);
-		DrawLine(y + 1, x + 1,false);
-		DrawLine(y + 1, x,true);
-		DrawLine(y + 1, x - 1,false);
-		DrawLine(y - 1, x + 1,false);
-		DrawLine(y - 1, x,true);
-		DrawLine(y - 1, x-1,false);
+		DrawLine(y, x + 1,horizontalLine);
+		DrawLine(y, x - 1,horizontalLine);
+		DrawLine(y + 1, x + 1,diognalLineLong);
+		DrawLine(y + 1, x,verticalLine);
+		DrawLine(y + 1, x - 1,diognalLineShort);
+		DrawLine(y - 1, x + 1,diognalLineShort);
+		DrawLine(y - 1, x,verticalLine);
+		DrawLine(y - 1, x-1,diognalLineLong);
 	}
 
 
 
-	void DrawLine(int x, int y,bool isThick)
+	void DrawLine(int x, int y,GameObject line)
 	{
 		try
 		{
@@ -46,12 +60,16 @@ public class firendLine : MonoBehaviour {
 				return;
 			if (Var.playerPos[y, x] != null)
 			{
-				
-				GameObject line = Instantiate(LineObj);
-				LineRenderer lr = line.GetComponent<LineRenderer>();
-				lr.sortingOrder = 0;
-				lr.SetPosition(0, birdScript.target);
-				lr.SetPosition(1, Var.playerPos[y, x].target);
+
+
+                Vector3 pos = (birdScript.transform.position + Var.playerPos[y,x].transform.position)/ 2f;
+                Quaternion rot = line.transform.rotation;
+                GameObject LineObj = Instantiate(line,pos, rot);
+                activeLines.Add(LineObj);
+                Var.playerPos[y, x].lines.activeLines.Add(LineObj);
+
+                //Deprecated code
+                /*
 			  
 				if (isThick)
 				{
@@ -62,8 +80,11 @@ public class firendLine : MonoBehaviour {
 				{
 					lr.startColor = thin;
 					lr.startColor = thin;
-				}
-				bool firstCrush = false;
+				}*/
+
+
+                //Relationship stuff. May revisit later
+                /*bool firstCrush = false;
 				bool secondCrush = false;
 				if (birdScript.relationshipBird != null && birdScript.relationshipBird.charName == Var.playerPos[y, x].charName)
 				{
@@ -81,15 +102,14 @@ public class firendLine : MonoBehaviour {
 				{
 					lr.startColor = relationship;
 					lr.endColor = relationship;                   
-				}
-				activeLines.Add(line);
-				Var.playerPos[y, x].lines.activeLines.Add(line);
+				}*/
 
 
 
 
 
-			}
+
+            }
 		}
 		catch {
 			
