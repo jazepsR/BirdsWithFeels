2ea5f398c39928e970246e6b32a609e0ca1350d4
Sebastian Larsson <Sebastian.Larsson.3738@student.uu.se>
2017-10-06 18:28:36 +0200

    Added additional animation to fight button
    use "active# when fight button is clickable! Will be iterated upon later
    - for now the logic works.

---
+++ Assets/particleAnimatorController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleAnimatorController : MonoBehaviour {
+
+    public ParticleSystem particles;
+
+
+	// Use this for initialization
+	void Start () {
+        if (particles == null)
+        {
+            particles = GetComponent<ParticleSystem>();
+        }
+        particles.Stop();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+public void play()
+    {
+        particles.Play();
+    }
+    public void stop()
+    {
+        particles.Stop(false);
+    }
+}
