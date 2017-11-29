05bfe882e1378fc9f40cbeb74c8b53bba99938bf
Sebastian Larsson <Sebastian.Larsson.3738@student.uu.se>
2017-11-29 16:15:23 +0100

    Minor animation polish, level design polish. Added link to feedback form in main menu

---
+++ Assets/webpageOpener.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class webpageOpener : MonoBehaviour {
+
+    public string webpageLink;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OpenWebpage()
+    {
+        Application.OpenURL(webpageLink);
+    }
+}
