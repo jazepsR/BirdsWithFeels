ac0cec2f9b3938d83c4c5b78fc68d39567d55b9a
Sebastian Larsson <Sebastian.Larsson.3738@student.uu.se>
2019-10-20 21:35:45 +0200

    Updated FinalBossBattleVultures to have better vulture placement and updated king graphivs. Added a script to control which side vultures are on with

---
+++ Assets/bossBattleVultureVisuals.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bossBattleVultureVisuals : MonoBehaviour
+{
+    public GameObject[] rightVultures;
+    public GameObject[] leftVultures;
+
+    public GameObject[] twoVulturekINGS;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < leftVultures.Length; i++)
+        {
+            leftVultures[i].gameObject.SetActive(false);
+        }
+        for (int i = 0; i < rightVultures.Length; i++)
+        {
+            rightVultures[i].gameObject.SetActive(false);
+        }
+
+
+    }
+
+    void setupLastBattle()
+    {
+        for (int i = 0; i < leftVultures.Length; i++)
+        {
+            leftVultures[i].gameObject.SetActive(false);
+        }
+
+        for (int i = 0; i < rightVultures.Length; i++)
+        {
+            rightVultures[i].gameObject.SetActive(true);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+if(        Input.GetKeyDown(KeyCode.A))
+        {
+            addVulturesToLeftSide();
+        }
+
+    }
+
+    
+    void addVulturesToLeftSide()
+    {
+
+
+        for (int i = 0; i < leftVultures.Length; i++)
+        {
+            if(leftVultures[i].gameObject.activeInHierarchy==false)
+            {
+                leftVultures[i].gameObject.SetActive(true);
+                break;
+            }
+            
+        }
+
+        for (int i = 0; i < rightVultures.Length; i++)
+        {
+            if (rightVultures[i].gameObject.activeInHierarchy == true)
+            {
+                rightVultures[i].gameObject.SetActive(false);
+                break;
+            }
+
+        }
+
+
+    }
+
+}
