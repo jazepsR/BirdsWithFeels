fd32e220de63f74e2b82d93ce2a190ac684cadd3
Jazeps <jazepsrutkis93@gmail.com>
2019-12-08 20:07:16 +0200

    *Added credits

---
+++ Assets/Scripts/UI/Credits.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    public RectTransform rect;
+    public float waitTime = 1.5f;
+    public float speed = 0.5f;
+    public float endPos = 332;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ScrollCredits());
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("mainMenu");
+        }
+    }
+    IEnumerator ScrollCredits()
+    {
+        yield return new WaitForSecondsRealtime(waitTime);
+        while(rect.position.y<endPos)
+        {
+            float posY = rect.position.y;
+            posY += speed * Time.deltaTime;
+            rect.position = new Vector3(rect.position.x, posY);
+            yield return null;
+        }
+        yield return new WaitForSecondsRealtime(waitTime);
+        SceneManager.LoadScene("mainMenu");
+    }
+}
