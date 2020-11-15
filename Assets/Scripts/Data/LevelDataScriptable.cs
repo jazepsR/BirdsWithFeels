8b4c9ffb2e7f6abcc75b6ebf6de060533e4dadb9
jazeps <jazepsrutkis93@gmail.com>
2020-11-15 16:15:11 +0100

    *Added level up text and sprite fields

--- Assets/Scripts/Data/LevelDataScriptable.cs
+++ Assets/Scripts/Data/LevelDataScriptable.cs
@@ -8,4 +8,7 @@ public class LevelDataScriptable : ScriptableObject
     public List<LevelBits> levelBits;
     public bool givesHeart = true;
     public bool givesPower = true;
+    [TextArea(5, 10)]
+    public string levelUpText;
+    public Sprite levelUpImage;
 }
