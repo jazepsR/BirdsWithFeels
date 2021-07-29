11b02f15376820e216304e4d0058f636f213cce8
Sebastian Larsson <Slebbastian@gmail.com>
2021-07-29 16:51:05 +0200

    Added BANDAGES to all birds

---
+++ Assets/BandageSwitcher.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BandageSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    public BandageSpriteData[] bandageSprites;
+
+    // Start is called before the first frame update
+    public void SetBandages(bool isHurt)
+    {
+        foreach(BandageSpriteData data in bandageSprites)
+        {
+            if(data.bandagedRenderer && data.healthySprite && data.bandagedSprite)
+            {
+                data.bandagedRenderer.sprite = isHurt ? data.bandagedSprite : data.healthySprite;
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class BandageSpriteData
+{
+    public Sprite healthySprite;
+    public Sprite bandagedSprite;
+    public SpriteRenderer bandagedRenderer;
+}
