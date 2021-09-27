319eebdc77d8614228389d4b1d06e4dc1d2084d4
Sebastian Larsson <Slebbastian@gmail.com>
2021-09-28 01:58:39 +0200

    Added some sick extra credits. Not finished visually lmao but the code is there

---
+++ Assets/Scripts/UI/Credits_person.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Credits_person : MonoBehaviour
+{
+    public string ContinueButtonLine;
+    Credits_special credits;
+    private Animator animator;
+    public Text buttonText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (ContinueButtonLine == null)
+        {
+            ContinueButtonLine = "next!";
+        }
+        if (buttonText != null)
+        {
+            buttonText.text = ContinueButtonLine;
+        }
+
+
+        animator = GetComponent<Animator>();
+    }
+
+    public void ShowPerson(Credits_special aCredits)
+    {
+        credits = aCredits;
+        animator.SetTrigger("show");
+    }
+
+    public void HidePerson()
+    {
+        animator.SetTrigger("hide");
+    }
+
+    public void ButtonClicked()
+    {
+        if (credits != null)
+        {
+            credits.ContinueButtonClicked(this);
+        }
+        else
+        {
+            print("invalid credits reference! Sorry :( ");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
