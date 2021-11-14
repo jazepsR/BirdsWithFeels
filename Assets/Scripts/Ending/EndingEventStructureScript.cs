0b6135d513826e2056bef7f3054af70bcad550da
Sebastian Larsson <Slebbastian@gmail.com>
2021-11-14 16:07:03 +0100

    Updated ending to have better dialogues, and better pacing, and restructured code to make it easier to work with

---
+++ Assets/Scripts/Ending/EndingEventStructureScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingEventStructureScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public EventScript EventToPlayBeforeBattle;
+    public Dialogue DialogueToPlayBeforeBattle;
+    public EventScript EventToPlayOnceBirdsArePlaced;
+    public bool VulturesShouldGoToLeftSideThisTurn;
+    public Var.Em[] EnemiesToSpawn;
+    public bool isFinalBattle;
+
+}
